22935fae60b6ab2040596d4a78d1812e2a16589e
Matheus <matheus.guedes.mg.m@gmail.com>
2025-10-10 21:52:46 -0300

    refactor:remoção do banco

--- mqtt_client/main.py
+++ mqtt_client/main.py
@@ -1,5 +1,7 @@
 import paho.mqtt.client as mqtt
+from paho.mqtt.enums import MQTTProtocolVersion
 import json
+from time import sleep
 
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Conectado: {reason_code}")
@@ -14,10 +16,19 @@ def on_message(client, userdata, msg):
     print(f"Unit: {payload["unit"]}")
     print(f"Timestamp: {payload["timestamp"]}")
 
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, protocol=MQTTProtocolVersion.MQTTv5)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
+mqttc.username_pw_set('tester', 'rosebud')
 
-mqttc.connect("127.0.0.1", 1883, 60)
+connected = False
+
+while(not connected):
+    try:
+        mqttc.connect("127.0.0.1", 1883, 60, '', 0, True)
+        connected = True
+    except ConnectionRefusedError:
+        print("Failed to connect. Retrying...")
+        sleep(5)
 
 mqttc.loop_forever()
