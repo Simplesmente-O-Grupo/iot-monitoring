e9f3d47100fd5f61342af0a6f8e1abbe3bea9093
Luiz Gustavo Monteiro <luizgu_mreis@hotmail.com>
2025-10-26 14:36:11 -0300

    Added database model

--- database/init_scripts/create_table.sql
+++ database/init_scripts/create_table.sql
@@ -1,3 +1,18 @@
+create table if not exists station (
+   id                integer,
+   name              varchar(100),
+   locationid        int null,
+   installation_date date
+);
+
+create table if not exists climameasures (
+   stationname  varchar(255),
+   measurevalue int null,
+   unit         float null,
+   currenttime  timestamp default current_timestamp
+);
+
+
 create table if not exists climameasures (
    stationname  varchar(255),
    measurevalue int null,
