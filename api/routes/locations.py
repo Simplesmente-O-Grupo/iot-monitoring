de7df7c80b2b0a2d2efe9c97463003771bc58bfa
Matheus <matheus.guedes.mg.m@gmail.com>
2025-11-03 09:21:37 -0300

    post location

--- api/routes/locations.py
+++ api/routes/locations.py
@@ -3,6 +3,8 @@ from sqlalchemy import select
 from ..database import SessionLocal
 from ..models import Location
 
+from pydantic import BaseModel
+
 router = APIRouter(
     prefix='/locations',
     tags=['locations']
@@ -25,3 +27,18 @@ async def get_locations():
     dc['size'] = len(dc['locations'])
     return dc
     
+
+class PostLocation(BaseModel):
+    street: str
+    avenue: str
+    zip_code:str
+
+@router.post('/')
+async def create_location(location: PostLocation):
+    session = SessionLocal()
+    session.begin()
+    session.add(Location(street=location.street, avenue=location.avenue, zip_code=location.zip_code))
+    session.commit()
+    session.close()
+    return {'msg': 'Localização adicionada com sucesso.'}
+
